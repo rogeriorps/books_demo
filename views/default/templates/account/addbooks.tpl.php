0384e8a965fd9d27d47fcd9bc81553ea252d1e3d
Rogerio Pimentel <rogerio.pimentel@freescale.com>
2014-06-09 21:25:05 -0300

    add php script upload file

--- views/default/templates/account/addbooks.tpl.php
+++ views/default/templates/account/addbooks.tpl.php
@@ -1,9 +1,20 @@
 
 <div id="main">
-<ul>
-<!-- START userbooks -->
-<li><a href="books/view/{bc_title}">{bc_title}</a></li>
-<!-- END userbooks -->
-</ul>
+    Adicionar livros
 
+    Adicionar 1 livro
+    
+    Adicionar vários livros (arquivos xls, xlsx, cvs)
+    
+    <form action="scripts/php/upload_file.php" method="post"
+enctype="multipart/form-data">
+<label for="file">Filename:</label>
+<input type="file" name="file" id="file"><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+
+    
+    Template xls
+    Template xlsx
+    Template cvs
  </div>
